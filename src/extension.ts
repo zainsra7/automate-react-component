c83b997763e69bfbe0d2c28c99549dfb11d9dca0
Zain Sra <zain.ulislam@skyscanner.net>
2019-12-15 19:12:06 +0000

    Added snippets, context menu, custom config, refactored code and added icon

--- src/extension.ts
+++ src/extension.ts
@@ -1,136 +1,112 @@
 import * as vscode from "vscode";
-const fs = require("fs");
 import {
   regex,
   getCamelCaseName,
   getPascalCaseName,
-  getComponentMainFileContent,
-  getComponentFixturesFileContent,
-  getComponentStylesFileContent,
-  getComponentTestFileContent,
-  getComponentIndexFileContent,
   getComponentNameFromUser,
   getDirectoryPathFromUser,
   createComponentDirectory,
   createComponentFile
 } from "./utils";
 
+interface FileProps {
+  fileName: string;
+  content: string;
+}
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand(
     "extension.createreactcomponent",
     async (uri: vscode.Uri) => {
       const currentlyOpenedWorkspace = vscode.workspace.workspaceFolders;
-      console.log(uri);
       if (currentlyOpenedWorkspace) {
         let pathFromRoot = "";
-
-        // Component activated via right click from explorer menu
-        if (uri) {
-          pathFromRoot = uri.fsPath;
-        } else {
-          pathFromRoot = currentlyOpenedWorkspace[0].uri.fsPath;
-        }
+        let componentCreatedSuccessfully = false;
 
         // Custom Configuration(s)
         const configurationFiles:
-          | object[]
+          | FileProps[]
           | undefined = vscode.workspace
           .getConfiguration("automatereact")
           .get("files");
 
-        if (!configurationFiles) {
+        // Component activated via right click from explorer menu
+        if (uri) {
+          pathFromRoot = uri.fsPath + "/";
         } else {
-          // Default behavior (Typescript React component with styles, fixtures, test, tsx and index file)
+          pathFromRoot = currentlyOpenedWorkspace[0].uri.fsPath + "/";
+        }
+        // Fetch and filter component Name
+        const componentName = await getComponentNameFromUser();
 
-          // Fetch and filter component Name
-          const componentName = await getComponentNameFromUser();
-
-          if (!componentName) {
-            vscode.window.showErrorMessage(
-              "Invalid component name! Please try again!"
-            );
-            return;
-          }
-
-          const componentNameSplitIntoArray =
-            componentName && componentName.match(regex);
-          let componentNameInCamelCase = "";
-          let componentNameInPascalCase = "";
-          if (componentNameSplitIntoArray) {
-            componentNameInCamelCase = getCamelCaseName(
-              componentNameSplitIntoArray
-            );
-            componentNameInPascalCase = getPascalCaseName(
-              componentNameSplitIntoArray
-            );
-          }
-
-          // Fetch and create component directory
-          const componentParentDirectoryPath = pathFromRoot;
-          if (!uri) {
-            const componentParentDirectoryPath = getDirectoryPathFromUser(
-              componentName,
-              pathFromRoot
-            );
-          }
-
-          const componentDirectoryPath = vscode.Uri.parse(
-            `${componentParentDirectoryPath}/${componentName}`
+        if (!componentName) {
+          vscode.window.showErrorMessage(
+            "Invalid component name! Please try again!"
           );
+          return;
+        }
 
+        const componentNameSplitIntoArray = componentName.match(regex);
+        let componentNameInCamelCase = "";
+        let componentNameInPascalCase = "";
+        if (componentNameSplitIntoArray) {
+          componentNameInCamelCase = getCamelCaseName(
+            componentNameSplitIntoArray
+          );
+          componentNameInPascalCase = getPascalCaseName(
+            componentNameSplitIntoArray
+          );
+        }
+
+        let componentDirectoryPath = vscode.Uri.parse(
+          `${pathFromRoot}${componentName}`
+        );
+        // Fetch and create component directory
+        if (!uri) {
+          const componentParentDirectoryPath = await getDirectoryPathFromUser(
+            componentName,
+            pathFromRoot
+          );
+          componentDirectoryPath = vscode.Uri.parse(
+            `${componentParentDirectoryPath}${componentName}`
+          );
+        }
+
+        try {
           await createComponentDirectory(componentDirectoryPath);
+        } catch (err) {
+          vscode.window.showInformationMessage(err);
+        }
 
-          // Create component files
+        // Create individual component files
+        if (configurationFiles && configurationFiles.length) {
+          configurationFiles.map(async file => {
+            let fileName = file.fileName
+              ? file.fileName.replace(/fn\$/gi, componentName)
+              : componentName + ".tsx";
 
-          const componentMainFilePath = vscode.Uri.parse(
-            componentDirectoryPath.fsPath + `/${componentName}.tsx`
-          );
-          const componentTestFilePath = vscode.Uri.parse(
-            componentDirectoryPath.fsPath + `/${componentName}.test.tsx`
-          );
-          const componentFixturesFilePath = vscode.Uri.parse(
-            componentDirectoryPath.fsPath + `/${componentName}.fixtures.ts`
-          );
-          const componentStylesFilePath = vscode.Uri.parse(
-            componentDirectoryPath.fsPath + `/${componentName}.styles.ts`
-          );
-          const componentIndexFilePath = vscode.Uri.parse(
-            componentDirectoryPath.fsPath + `/index.ts`
-          );
+            let fileContent = file.content
+              ? file.content
+                  .replace(/cN\$/g, componentNameInCamelCase)
+                  .replace(/Cn\$/g, componentNameInPascalCase)
+                  .replace(/fn\$/gi, componentName)
+              : "";
 
-          await createComponentFile(
-            componentMainFilePath,
-            getComponentMainFileContent(
-              componentNameInPascalCase,
-              componentName
-            )
-          );
-          await createComponentFile(
-            componentTestFilePath,
-            getComponentTestFileContent(
-              componentNameInCamelCase,
-              componentNameInPascalCase,
-              componentName
-            )
-          );
-          await createComponentFile(
-            componentFixturesFilePath,
-            getComponentFixturesFileContent(componentNameInCamelCase)
-          );
-          await createComponentFile(
-            componentStylesFilePath,
-            getComponentStylesFileContent(componentNameInPascalCase)
-          );
-          await createComponentFile(
-            componentIndexFilePath,
-            getComponentIndexFileContent(componentName)
-          );
+            const componentFilePath = vscode.Uri.parse(
+              componentDirectoryPath.fsPath + `/${fileName}`
+            );
+            await createComponentFile(componentFilePath, fileContent);
+          });
+          componentCreatedSuccessfully = true;
+        }
+        if (componentCreatedSuccessfully) {
           vscode.window.showInformationMessage(
             `${componentName} created successfully under ${componentDirectoryPath.fsPath}!`
           );
         }
       } else {
-        vscode.window.showErrorMessage("Open workspace/project and try again!");
+        vscode.window.showErrorMessage(
+          "Open a workspace/project and try again!"
+        );
       }
     }
   );
--- src/utils.ts
+++ src/utils.ts
@@ -4,74 +4,6 @@ const fs = require("fs");
 //Shoutout to https://levelup.gitconnected.com/converting-a-string-to-camelcase-in-javascript-f88a646a22b4
 export const regex = /[A-Z\xC0-\xD6\xD8-\xDE]?[a-z\xDF-\xF6\xF8-\xFF]+|[A-Z\xC0-\xD6\xD8-\xDE]+(?![a-z\xDF-\xF6\xF8-\xFF])|\d+/g;
 
-export const getComponentMainFileContent = (
-  componentNameInPascalCase: string,
-  componentNameInput: string
-) => {
-  return `import React from 'react';
-import { ${componentNameInPascalCase}Styles } from './${componentNameInput}.styles';
-
-interface Props{
-  name: string;
-};
-
-export const ${componentNameInPascalCase} = ({ name }: Props) => {
-  const classes = ${componentNameInPascalCase}Styles({});
-
-  return(
-    <>\${name}</>
-  );
-};`;
-};
-
-export const getComponentStylesFileContent = (
-  componentNameInPascalCase: string
-) => {
-  return `import { createStyles, makeStyles } from '@material-ui/core';
-import { COLOURS } from '../../../utils/constants';
-
-export const ${componentNameInPascalCase}Styles = makeStyles(() => 
-  createStyles({
-    root: {
-      display: 'flex',
-    },
-  }),
-);`;
-};
-
-export const getComponentTestFileContent = (
-  componentNameInCamelCase: string,
-  componentNameInPascalCase: string,
-  componentNameInput: string
-) => {
-  return `import React from 'react';
-import 'jest-dom/extend-expect';
-import { cleanup, wait } from '@testing-library/react';
-import { renderWithRouter } from '../../../../../test/utils/render-with-router';
-import { MockProviders } from '../../../../../test/utils/mock-providers';
-import { ${componentNameInCamelCase}Fixtures } from './${componentNameInput}.fixtures';
-import { ${componentNameInPascalCase} } from './${componentNameInput}';
-
-describe('${componentNameInput} Tests', () => {
-  it('component renders correctly', () => {
-    const { getByTestId, findByText } = renderWithRouter(
-      <MockProviders>
-        <${componentNameInPascalCase} name={${componentNameInCamelCase}Fixtures.name}/>
-      </MockProviders>
-    );
-  });
-});
-`;
-};
-
-export const getComponentFixturesFileContent = (
-  componentNameInCamelCase: string
-) => {
-  return `export const ${componentNameInCamelCase}Fixtures = {
-    name: 'Test Component',
-  };`;
-};
-
 export const getComponentNameFromUser = async () => {
   const componentName = await vscode.window.showInputBox({
     value: "",
@@ -86,38 +18,24 @@ export const getComponentNameFromUser = async () => {
   });
   return componentName ? componentName.toLowerCase() : componentName;
 };
-export const getComponentIndexFileContent = (componentName: string) => {
-  return `export * from './${componentName}';`;
-};
-
-function isDir(path: string) {
-  try {
-    var stat = fs.lstatSync(path);
-    return stat.isDirectory();
-  } catch (e) {
-    return false;
-  }
-}
 
 export const getDirectoryPathFromUser = async (
   componentName: string,
-  path: string
+  pathFormRoot: string
 ) => {
-  const fileName = await vscode.window.showInputBox({
-    value: "/",
+  const directoryName = await vscode.window.showInputBox({
+    value: pathFormRoot,
     placeHolder:
       "Please enter *relative* directory path for your component: e.g. `/client/src/components/` ",
-    validateInput: text => {
-      vscode.window.showInformationMessage(`Validating: ${path + text}`);
-      let validDirectory = isDir(path + text);
-      return validDirectory === false
-        ? "Please enter a valid directory value"
-        : null;
-    },
     ignoreFocusOut: true,
-    prompt: `Examples: client/src/components will create a folder ${componentName} and add relevant files in it`
+    prompt: `Examples: client/src/components will create a folder ${componentName} and add relevant files in it`,
+    valueSelection: [pathFormRoot.length, pathFormRoot.length]
   });
-  return path + fileName;
+  return directoryName
+    ? directoryName.endsWith("/")
+      ? directoryName
+      : directoryName + "/"
+    : "";
 };
 
 export const getCamelCaseName = (componentNameSplitArray: RegExpMatchArray) => {
@@ -154,12 +72,12 @@ export const getPascalCaseName = (
 export const createComponentDirectory = async (
   componentDirectoryPath: vscode.Uri
 ) => {
-  vscode.workspace.fs.createDirectory(componentDirectoryPath);
+  await vscode.workspace.fs.createDirectory(componentDirectoryPath);
 };
 export const createComponentFile = async (
   componentFilePath: vscode.Uri,
   content: string
 ) => {
   await vscode.workspace.fs.writeFile(componentFilePath, new Uint8Array([]));
-  fs.writeFileSync(componentFilePath.fsPath, content, "utf8");
+  await fs.writeFileSync(componentFilePath.fsPath, content, "utf8");
 };
